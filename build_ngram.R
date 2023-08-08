29f45c34028e1ea9e3eebcfdfc2994427afbd6b0
Sandile Tshazibana <128171150+Tshas1@users.noreply.github.com>
2023-08-08 16:35:53 +0200

    Add files via upload

---
+++ build_ngram.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(tau)
+library(tm)
+library(hash)
+
+# load data
+# data availabel here https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip
+message("load data...")
+message(Sys.time())
+setwd(getSrcDirectory(function(){}))
+blog.full <- readLines("data/en_US/en_US.blogs.txt")
+news.full <- readLines("data/en_US/en_US.news.txt")
+twit.full <- readLines("data/en_US/en_US.twitter.txt")
+# batch size
+nbatch <- 50
+blog.len <- ceiling(length(blog.full) / nbatch)
+news.len <- ceiling(length(news.full) / nbatch)
+twit.len <- ceiling(length(twit.full) / nbatch)
+
+# preprocessing functions
+removeURL <- function(x) gsub("http\\S+", "", x)
+removeHash <- function(x) gsub("[@#&]\\S+", "", x)
+# don't know how to remove location, like "@ los angeles"
+removeNumPunct <- function(x) gsub("[^A-z[:space:]']*", "", x)
+# it turns out that Japanese characters belong to [:alpha:], but not [A-z]. Absurd.
+h <- hash()
+
+# loop over batches
+for (b in 1:nbatch - 1) {
+# for (b in 12932) {
+  message(sprintf("Processing the %i-th batch", b))
+  
+  # concatenate text and preprocess
+  blog <- blog.full[blog.len * b + (1:blog.len)]
+  news <- news.full[news.len * b + (1:news.len)]
+  twit <- twit.full[twit.len * b + (1:twit.len)]
+  bnt <- c(blog, news, twit) %>% 
+    removeURL() %>% removeHash() %>% 
+    removeNumPunct() %>% tolower() %>% stripWhitespace()
+  
+  # obtain trigram
+  trigram <- textcnt(bnt, n=3, split=" ", method="string", decreasing = TRUE) 
+  
+  # create hash table for fast prediction
+  for (i in 1:length(trigram)) {
+    if (trigram[i] < 4) {
+      break
+    } else {
+      gram <- strsplit(names(trigram[i]), split = ' ')[[1]]
+      history <- paste0(gram[1:2], collapse = ' ')
+      candidate <- gram[3]
+      count <- trigram[[i]]
+      if (candidate %in% h[[history]]$candidate) {
+        index <- h[[history]]$candidate == candidate
+        h[[history]]$count[index] <- h[[history]]$count[index] + count
+      } else {
+        h[[history]]$candidate <- c(h[[history]]$candidate, candidate)
+        h[[history]]$count <- c(h[[history]]$count, count)
+      }
+    }
+  }
+  gc()
+}
+
+if (1) {
+  save(h, file = "hashtable.Rdata")
+  save(h, file = "wordpred/hashtable.Rdata")
+  save(trigram, file = "trigram.Rdata")
+}
---
+++ wordpred/app.R
@@ -0,0 +1,69 @@
+library(shiny)
+library(hash)
+load("hashtable.Rdata")
+
+ui <- fluidPage(
+  
+  titlePanel("Word Prediction via Ngram"),
+  
+  hr(),
+  
+  fluidRow(
+    column(5, textInput("userInput", "Please type here ... type at least 
+                        two words to enable prediction.")),
+    column(7, h3(verbatimTextOutput(outputId = "distPrediction")))
+  ),
+  
+  hr(),
+  
+  fluidRow(
+    column(8, h3("What is this?"),
+              h4("This app predicts the next word you will most probably type, based on 
+                 previous two words that you already typed. This app uses a tri-gram model
+                 that is trained over ~500 MB text data from blogs, news articles and 
+                 twitter. Get the source code ", 
+                 tags$a(href="https://github.com/chunjiw/wordpred", "here!")))
+  ),
+  
+  # hr(),
+  # 
+  # fluidRow(
+  #   column(8, h3("Join my webinar!"),
+  #          h4("As a member of LEAP-SIG-DS (Special Interest Group of Data Science), 
+  #             I will host a webinar on 14th Sep, Thursday at 9pm to share my experience 
+  #             making this app."),
+  #          h4(tags$a(href="https://zoom.us/j/465955864", "Join us!")))
+  # ),
+  
+  hr(),
+  h4("Made by Chunji Wang")
+
+)
+
+server <- function(input, output) {
+  output$distPrediction <- renderText({
+    userInput <- trimws(input$userInput)
+    userInput <- gsub('\\s+', ' ', userInput)
+    pred.sentense <- paste(userInput, "...")
+    words <- strsplit(tolower(userInput), split = ' ')[[1]]
+    if (length(words) >= 2) {
+      history <- paste0(tail(words, 2), collapse = ' ')
+      candidates <- h[[history]]$candidate
+      candidates[candidates == 'i'] <- 'I'
+      if (length(candidates)) {
+        maxDisplay <- min(5, length(candidates))
+        for (i in 1:maxDisplay) {
+          pred.sentense <- paste(pred.sentense,
+                                 paste(userInput, candidates[i], sep = ' '),
+                                 sep = '\n')
+        }
+      }
+      pred.sentense
+    } else {
+      userInput
+    }
+    })
+  
+}
+
+shinyApp(ui = ui, server = server)
